85218234725455ebf65c97ee7b5a21ff3caebeff
Rafajorda <rafajorgis@gmail.com>
2025-12-16 19:24:11 +0100

    Add estate_account module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate - Accounting Link',
+    'version': '0.1',
+    'summary': 'Create invoices when properties are sold',
+    'description': 'Link module between Real Estate and Accounting: create invoices on sold properties.',
+    'author': 'Rafael Jorda',
+    'depends': ['real_estate', 'account'],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+}
